2a67b74dec3cdcf734c18df2fd1de93751f5e18e
Aashish <aashishs2002@gmail.com>
2023-05-11 21:59:17 +0530

    Add files via upload

---
+++ java/java mini_proj/nqueens/src/nqueens/nq.java
@@ -0,0 +1,67 @@
+package nqueens;
+
+public class nq {
+	final int N = 4;
+	  
+    /* A utility function to print solution */
+    void printSolution(int board[][])
+    {
+    	System.out.println("The Solution for the N-Queens Problem is:");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++)
+                System.out.print(" " + board[i][j]
+                                 + " ");
+            System.out.println();
+        }
+    }
+  
+    boolean isSafe(int board[][], int row, int col)
+    {
+        int i, j;
+        for (i = 0; i < col; i++)
+            if (board[row][i] == 1)
+                return false;
+        for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+            if (board[i][j] == 1)
+                return false;
+        for (i = row, j = col; j >= 0 && i < N; i++, j--)
+            if (board[i][j] == 1)
+                return false;
+  
+        return true;
+    }
+    boolean solveNQUtil(int board[][], int col)
+    {
+        if (col >= N)
+            return true;
+        for (int i = 0; i < N; i++) {
+            if (isSafe(board, i, col)) {
+                board[i][col] = 1;
+                if (solveNQUtil(board, col + 1) == true)
+                    return true;
+                board[i][col] = 0; // BACKTRACK
+            }
+        }
+        return false;
+    }
+    boolean solveNQ()
+    {
+        int board[][] = { { 0, 0, 0, 0 },
+                          { 0, 0, 0, 0 },
+                          { 0, 0, 0, 0 },
+                          { 0, 0, 0, 0 } };
+  
+        if (solveNQUtil(board, 0) == false) {
+            System.out.print("Solution does not exist");
+            return false;
+        }
+  
+        printSolution(board);
+        return true;
+    }
+    public static void main(String args[])
+    {
+        nq Queen = new nq();
+        Queen.solveNQ();
+    }
+}
